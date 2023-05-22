6089d318f2beb289267faa7059e4aa2f96850444
Abundancia <abundancia@MacBook-Air-de-Abundancia.local>
2023-05-22 13:33:18 +0200

    first commit

---
+++ jour01/job01/index.php
@@ -0,0 +1,32 @@
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/runtrack2/jour01/favicon-32x32.ico">
+    <title>Document</title>
+</head>
+<body>
+    <?php $str = "LaPlateforme";
+
+    ?>
+      <?php $str2 = "Vive";
+ 
+    ?>
+      <?php $str3 = "!";
+    echo "$str2  $str  $str3" ;
+    ?>
+    <?php
+    $val = 6;
+    echo ($val+4);
+    ?>
+    <?php
+    boolval(true)
+    ?>
+    <?php
+    boolval(false)
+    ?>
+</body>
+</html>
