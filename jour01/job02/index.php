d15b574461fc4bb02b93ea8c87aa839470603ae7
Abundancia <abundancia@MacBook-Air-de-Abundancia.local>
2023-05-22 14:26:36 +0200

    commentaire job02

---
+++ jour01/job02/index.php
@@ -0,0 +1,20 @@
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/runtrack2/jour01/job02/favicon.ico">
+    <title>job02</title>
+</head>
+<body>
+<?php 
+echo "Hello La Plateforme";
+echo "<br>";
+/*echo "Hello La Plateforme";
+
+
+?>
+</body>
+</html>
