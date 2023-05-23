fc0c42aedf4574dc4b66c7530fbc9b2ac48286bb
Abundancia <abundancia@MacBook-Air-de-Abundancia.local>
2023-05-23 11:06:29 +0200

    exclusion des chiffres

---
+++ jour02/job02.php
@@ -0,0 +1,26 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=ph, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$min = 0;
+$max = 1337;
+$i = $min;
+
+while ($i <= $max) {
+    if ($i != 26 && 37 && 88 && 1111 && 3233 ) {
+        echo  $i . '<br />';
+    }
+    $i++;
+ }
+
+?>
+ 
+
+
+</body>
+</html>
