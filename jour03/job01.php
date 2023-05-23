0d3c31697a8df2a21a3d18f8fb225eceda27ef30
Abundancia <abundancia@MacBook-Air-de-Abundancia.local>
2023-05-23 14:52:17 +0200

    Tableau avec fonction pairity

--- jour03/job01.php
+++ jour03/job01.php
@@ -1,42 +1,34 @@
-Créez un tableau contenant les nombres 200, 204, 173, 98, 171, 404, 459.
-Parcourez ce tableau et affichez pour chaque nombre “X est paire<br />” ou “X est
-impaire<br />”, sur votre page index.php. X prenant tour à tour chacune des valeurs
-comprises dans ce tableau.
-Ex. : 200 est paire
-204 est paire
-173 est impaire
-98 est paire
-171 est impaire
-404 est paire
-459 est impaire
-
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <?php
-// $NbrCol : le nombre de colonnes
-// $NbrLigne : le nombre de lignes
-// -------------------------------------------------------
-echo '<table>';
-for ($i=1; $i<=$NbrLigne; $i++) {
-   echo '<tr>' . 200 . 204 . 173 . 98 . 171 . 404 . 459 ;
-   for ($j=1; $j<=$NbrCol; $j++) {
-         echo '<td>';
-          // ------------------------------------------
-          // AFFICHAGE ligne $i, colonne $j
-         echo $affichage;
-          // ------------------------------------------
-         echo '</td>';
-   }
-   echo '</tr>';
-   $j=1;
-}
-echo '</table>';
-?>
+    <table border = "1">
+        <tr>
+            <th>Nombre</th>
+            <th>Pairity</th>
+        </tr>
+        <?php
+        $numbers = [200, 204, 173, 98, 171, 404, 459];
+        
+        foreach ($numbers as $number) {
+            echo '<tr>';
+            echo '<td>' . $number . '</td>';
+            echo '<td>';
+            if ($number % 2 == 0) {
+                echo 'PAIRE';
+            } else {
+                echo 'IMPAIRE';
+            }
+            echo '</td>';
+            echo '</tr>';
+        }
+        ?>
+     </table>
 </body>
 </html>
