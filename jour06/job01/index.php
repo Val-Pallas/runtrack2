158cf1947dd98f896f95c8800735bc92b410c3fc
Abundancia <abundancia@MacBook-Air-de-Abundancia.local>
2023-05-30 15:03:14 +0200

    function 1er commit

---
+++ jour06/job01/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="index.css" rel="stylesheet">
+    <title>Job01</title>
+</head>
+<body>
+    <P>Créez une fonction nommée “hello()”. Cette fonction, une fois appelée, doit afficher sur
+la page : “Hello LaPlateforme!”.</P>
+<?php
+function hello(){
+    echo "Hello LaPlateforme";
+}
+hello();
+?>
+</body>
+</html>
