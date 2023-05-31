f0841cb545a52f546b133c714408e6620de0f1dd
Abundancia <abundancia@MacBook-Air-de-Abundancia.local>
2023-05-31 12:58:07 +0200

    Ocurrences

--- jour06/job05/test.php
+++ jour06/job05/test.php
@@ -1,15 +1,11 @@
+
 <?php
 function occurrences(){
     $str = $_POST["frase"];
     
-    $charCounts = count_chars($str, 1) ;
-
-    foreach ($charCounts as $char => $count) {
-       $out =  "Il y a $count occurence(s) de \"" . chr($char). "\" dans la phrase.\n";
+    foreach (count_chars($str, 1) as $i => $val) {
+        echo "Il y a $val occurence(s) de \"" , chr($i) , "\" dans la phrase.\n";
     }
-    return $out ;
 }
-
 echo occurrences();
 ?>
-
